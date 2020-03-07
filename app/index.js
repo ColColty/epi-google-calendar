17b7837e6ca46b90c65d899bd6a8a337d332c6f3
Tomàs Forné <tomas.forne-cappeau@epitech.eu>
2020-03-07 22:43:27 +0100

    feat: Add events to database to know if already added

--- app/index.js
+++ app/index.js
@@ -1,49 +1,48 @@
 const googleAuth = require('./src/google-oauth/google-oauth');
 const {login, getEvents} = require('./src/epitech-calendar/epitech-calendar');
-const {google} = require('googleapis');
 const dotenv = require('dotenv');
 dotenv.config();
+const cron = require('node-cron');
+const MongoClient = require('mongodb').MongoClient;
+const URL_DB = "mongodb://mongo:27017/";
+const {addEventGC} = require('./src/google-calendar/google-calendar');
 
-
-const eventTemplate = ({title, room, start, end, color}) => ({
-    'summary': title,
-    'description': 'This course take place in the room: ' + room,
-    'start': {
-        'dateTime': start.toISOString(),
-        'timeZone': process.env.TIME_ZONE || 'Europe/Paris'
-    },
-    'end': {
-        'dateTime': end.toISOString(),
-        'timeZone': process.env.TIME_ZONE || 'Europe/Paris'
-    },
-    'colorId': color
-});
-
-const CALENDAR_ID = process.env.CALENDAR_ID;
 const IMPORTANT_EVENT_COLOR_ID = process.env.IMPORTANT_EVENT_COLOR_ID;
+const IMPORTANT_EVENTS_NAMES = ["Delivery", "Follow-up", "Evaluation", "Review"];
 
-async function main(auth) {
-    await login();
 
-    const activities = await getEvents(new Date().toISOString().split('T')[0], '2020-06-30');
+MongoClient.connect(URL_DB, (err, db) => {
+    if (err) {
+        console.log("Can't connect to Database.");
+        throw err;
+    }
+    console.log("Connected to DB !");
 
-    const calendar = google.calendar({version: 'v3', auth});
+    const dbo = db.db('epi-google-calendar');
 
-    activities.map(el => {
-        if (el.title.includes('Delivery') || el.title.includes('Follow-up') || el.title.includes('Evaluation') || el.title.includes('Review')) {
-            el.color = IMPORTANT_EVENT_COLOR_ID;
-        } else {
-            el.color = undefined;
-        }
-        calendar.events.insert({
-            auth: auth,
-            calendarId: CALENDAR_ID,
-            resource: eventTemplate(el)
-        }, function(err, event) {
-            if (err) return console.error('There was an error contacting the Calendar service:', err);
-            console.log('Event created:', event.data);
-        });
+    dbo.createCollection('events', (err, res) => {
+        if (err) throw err;
     })
-}
 
-googleAuth(main);
+    async function main(auth) {
+        await login();
+
+        const activities = await getEvents(new Date().toISOString().split('T')[0], '2020-06-30');
+
+        activities.map(el => {
+            if (new RegExp(IMPORTANT_EVENTS_NAMES.join("|")).test(el.title)) {
+                el.color = IMPORTANT_EVENT_COLOR_ID;
+            } else {
+                el.color = undefined;
+            }
+            el.start = new Date(el.start).toISOString();
+            el.end = new Date(el.end).toISOString();
+
+            addEventGC(el, dbo, auth);
+        });
+    }
+
+    cron.schedule('40 * * * * *', () => {
+        googleAuth(main);
+    });
+})
